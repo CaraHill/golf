3e709bd2adeedc18276699109fc9f61209f86711
Cara Hill <cara@rabidtech.co.nz>
2020-08-16 21:06:08 +1200

    Added ability to enter inputs on each row. Hardcoded 2d array means only keeping score for player 1. Will fix up to be dynamic next time.

--- src/index.js
+++ src/index.js
@@ -47,17 +47,64 @@ class GameInfo extends React.Component {
 }
 
 class PlayerTable extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      scores: [[0],[0],[0],[0],[0],[0],[0],[0],[0]]
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // scoresInitializer() {
+  //   console.log(this.props)
+  //   if(this.props) {
+  //     let scoresArray = [];
+  //     for(let i=1; i < 10; i++) {
+  //       let holeArray = []
+  //       for(let i=0; i < this.props.number; i++) {
+  //         holeArray.push(
+  //           0
+  //         )
+  //       }
+  //       scoresArray.push(holeArray);
+  //     }
+  //     console.log(scoresArray)
+  //     return scoresArray;
+  //   } else {
+  //     return [];
+  //   }
+  // }
+
+  handleChange({ target }) {
+    let holeIndex = target.dataset.holeNumber;
+    let playerIndex = target.dataset.playerNumber;
+
+    this.setState(state => {
+      state.scores[holeIndex][playerIndex] = target.value;
+
+      return {
+        scores: state.scores
+      };
+    });
+  }
+
   holesTable() {
     const numberHoles = [];
-
     for(let i=1; i < 10; i++) {
       numberHoles.push(
         <tr>
           <td>{i}</td>
-          {this.playersInputs()}
+          {this.playersInputs(i-1, this.state.scores[i-1])}
         </tr>
       )
     }
+    numberHoles.push(
+      <tr>
+        <td>Total</td>
+        {this.playersTotals()}
+      </tr>
+    )
 
     return numberHoles;
   }
@@ -74,12 +121,36 @@ class PlayerTable extends React.Component {
     return numberPlayers;
   }
 
-  playersInputs() {
+  playersTotals() {
+    const totalValues = [];
+
+    for(let i=1; i <= this.props.number; i++) {
+      let playerScores = this.state.scores.map(x => x[i-1]);
+      const reducer = (accumulator, currentValue) => parseInt(accumulator) + parseInt(currentValue);
+      totalValues.push(
+        <td>
+          {playerScores.reduce(reducer)}
+        </td>
+      )
+    }
+
+    return totalValues;
+  }
+
+  playersInputs(holeIndex, scores) {
     const numberInputs = [];
 
     for(let i=1; i <= this.props.number; i++) {
       numberInputs.push(
-        <td>0</td>
+        <td><input
+          type="number"
+          name="playerScores"
+          placeholder="0"
+          value={scores[i-1]}
+          onChange={ this.handleChange }
+          data-player-number={i-1}
+          data-hole-number={holeIndex}
+        /></td>
       )
     }
 
